5fd536b71312740094df687fc579011d2be1abeb
Kristen Campbell <kristenc1031@gmail.com>
2021-01-30 20:34:13 -0700

    fixed js

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Work.Day.Scheduler
 
 Scheduled business hours 8:00a-6:00p
+Click the save icon to save appointment
--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-// $(document).ready(function() {
-//     var isSaved = false;
-//     var text = "textarea"
-//     var time = "id"
-// });
-//   $(".saveBtn").on("click", function(){
-//      if (isSaved) {
-//        return true;
-//      }
-//   });
+$(document).ready(function() {
+    var isSaved = false;
+    var text = "textarea"
+    var time = "id"
+});
+  $(".saveBtn").on("click", function(){
+     if (isSaved) {
+       return true;
+     }
+  });
     
 
 
-$(document).ready(function() {
 
-    var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do"));
-      var isSaved = false;
-      var text = "textarea"
-      var time = "id"
-    });
-      $(".btn saveBtn").on("click", function(){
-      if (isSaved) {
-        return true;
-     }
-    });
     
